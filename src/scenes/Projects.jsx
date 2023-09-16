2f3b643dd5906163d947d992c3f40e92a363f76d
snoopyods <snoopyods@gmail.com>
2023-09-16 01:16:34 -0700

    Updated layout and messaging

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -15,7 +15,14 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ title }) => {
+const projectDescriptions = [
+  "An Admin Dashboard where I first used MUI!",
+  "Had some fun with pygames and made a Pokémon Ketchum All.",
+  "Passion Project where I experimented with MERN and antd.",
+  "An ECommerce site where I got to utilize Strapi and Stripe!",
+];
+
+const Project = ({ title, description }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
@@ -25,8 +32,7 @@ const Project = ({ title }) => {
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
         <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
+          {description}
         </p>
       </div>
       <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
@@ -58,9 +64,7 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
-          purus nec eget eleifend ut elit.
+          Here are a few of my recent projects I enjoyed working on.
         </p>
       </motion.div>
 
@@ -80,17 +84,12 @@ const Projects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" />
-          <Project title="Project 2" />
+          <Project title="Project 1" description={projectDescriptions[0]} />
+          <Project title="Project 2" description={projectDescriptions[1]} />
 
           {/* ROW 2 */}
-          <Project title="Project 3" />
-          <Project title="Project 4" />
-          <Project title="Project 5" />
-
-          {/* ROW 3 */}
-          <Project title="Project 6" />
-          <Project title="Project 7" />
+          <Project title="Project 3" description={projectDescriptions[2]} />
+          <Project title="Project 4" description={projectDescriptions[3]} />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
